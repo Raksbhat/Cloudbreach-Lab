f5adfdabeb0609e7d7a8bf2e9b5e36eed1e646b3
Rakshit Bhat <95375478+Raksbhat@users.noreply.github.com>
2026-09-15 14:45:46 +0000

    Synchronize Terraform with existing AWS lab infrastructure

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,7 +49,7 @@ resource "aws_route_table" "public" {
 
 resource "aws_route" "internet" {
   route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "13.48.85.149/32"
+  destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.lab.id
 }
 
@@ -64,7 +64,7 @@ resource "aws_security_group" "open_ssh" {
   vpc_id      = aws_vpc.lab.id
 
   ingress {
-    description = "SSH from anywhere"
+    description = "SSH from trusted IP"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -75,7 +75,7 @@ resource "aws_security_group" "open_ssh" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["13.48.85.149/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -93,59 +93,9 @@ resource "aws_instance" "lab" {
     aws_security_group.open_ssh.id
   ]
 
-  associate_public_ip_address = true
+  associate_public_ip_address = false
 
   tags = {
     Name = "cloudbreach-lab-ec2"
-  }
-}
-
-resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
-  name              = "/cloudbreach-lab/vpc-flow-logs"
-  retention_in_days = 7
-}
-
-resource "aws_iam_role" "vpc_flow_logs" {
-  name = "cloudbreach-lab-vpc-flow-logs"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "vpc-flow-logs.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy" "vpc_flow_logs" {
-  role = aws_iam_role.vpc_flow_logs.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Action = [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents",
-        "logs:DescribeLogGroups",
-        "logs:DescribeLogStreams"
-      ]
-      Resource = "*"
-    }]
-  })
-}
-
-resource "aws_flow_log" "lab" {
-  vpc_id          = aws_vpc.lab.id
-  traffic_type    = "ALL"
-  iam_role_arn    = aws_iam_role.vpc_flow_logs.arn
-  log_destination = aws_cloudwatch_log_group.vpc_flow_logs.arn
-
-  tags = {
-    Name = "cloudbreach-lab-vpc-flow-logs"
   }
 }
